fc35653cd416c90c482c6976f714527fa133c426
OtabekVaxobov <elot2017@mail.ru>
2023-09-24 22:13:46 +0500

    fix: midleware 3

--- middleware.ts
+++ middleware.ts
@@ -3,10 +3,10 @@ import { NextRequest, NextResponse } from "next/server";
 
 export default async function middleware(req: NextRequest) {
   // Get the pathname of the request (e.g. /, /protected)
-  const path = req.url;
+  const url = req.url;
 
   // If it's the root path, just render it
-  if (path === "/") {
+  if (url === "/") {
     return NextResponse.next();
   }
     
@@ -19,9 +19,9 @@ export default async function middleware(req: NextRequest) {
   //   secret: process.env.NEXTAUTH_SECRET,
   // });
 
-  if (!session && path === "/protected") {
+  if (!session && url === "/protected") {
     return NextResponse.redirect(new URL("/login", req.url));
-  } else if (await session && (path === "/login" || path === "/register")) {
+  } else if (await session && (url === "/login" || url === "/register")) {
     return NextResponse.redirect(new URL("/protected", req.url));
   }
   return NextResponse.next();
